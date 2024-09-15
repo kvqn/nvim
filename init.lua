e1bd096ac314a8b312763add1de7c1ca39064ed0
kvqn <guneetaggarwal@gmail.com>
2024-09-15 19:39:37 +0530

    feat: added some plugins

--- init.lua
+++ init.lua
@@ -432,10 +432,15 @@ require('lazy').setup({
         markdown = { 'mdformat' },
         zsh = { 'shfmt' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'isort', 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        ruff = {
+          command = 'ruff format',
+        },
       },
     },
   },
